91baae9fecbe17a8191d40333a857313f28e733b
Sam <samuel_lawrence@outlook.com>
2023-03-09 01:35:23 +0000

    new react form

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
 import React, { useState } from 'react';
-import { Button, TextField } from '@mui/material';
-
-interface Product {
-  id: string;
-  name: string;
-  price: number;
-  productType: string;
-}
-
-interface FormData {
-  name: string;
-  email: string;
-  phone: string;
-  products: Product[];
-  discount: string;
-}
+import {
+  Alert,
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Rating,
+  Select,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { Transaction, Customer, MemberType, Product } from './models/models';
+import Info from './components/info';
 
 const App: React.FC = () => {
-  const [formData, setFormData] = useState<FormData>({
+  const [formData, setFormData] = useState<Transaction>({
     name: '',
     email: '',
     phone: '',
     products: [],
     discount: '',
+    status: Customer.RETAIL,
+    memberType: MemberType.MEMBER,
+    review: 0,
+    fraud: false,
+    location: '',
   });
 
+  const [alert, setAlert] = useState(false);
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
     const url = process.env.REACT_APP_API_GATEWAY_URL;
+
     try {
       const response = await fetch(url + 'events', {
         mode: 'no-cors',
@@ -37,6 +43,20 @@ const App: React.FC = () => {
       });
       const data = await response.json();
       console.log(data);
+      setAlert(true);
+
+      setFormData({
+        name: '',
+        email: '',
+        phone: '',
+        products: [],
+        discount: '',
+        status: Customer.RETAIL,
+        memberType: MemberType.MEMBER,
+        review: 0,
+        fraud: false,
+        location: '',
+      });
     } catch (error) {
       console.error(error);
     }
@@ -44,7 +64,7 @@ const App: React.FC = () => {
 
   const handleInputChange = (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-    key: keyof FormData,
+    key: keyof Transaction,
   ) => {
     setFormData({ ...formData, [key]: event.target.value });
   };
@@ -74,70 +94,191 @@ const App: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <TextField
-        label="Name"
-        value={formData.name}
-        onChange={(event) => handleInputChange(event, 'name')}
-      />
-      <br />
-      <TextField
-        label="Email"
-        value={formData.email}
-        onChange={(event) => handleInputChange(event, 'email')}
-      />
-      <br />
-      <TextField
-        label="Phone"
-        value={formData.phone}
-        onChange={(event) => handleInputChange(event, 'phone')}
-      />
-      <br />
+      {alert && (
+        <Alert
+          severity="success"
+          onClose={() => {
+            setAlert(false);
+          }}
+        >
+          Product submission successful!
+        </Alert>
+      )}
+
+      <Typography align="center" variant="h4" color="primary" gutterBottom>
+        AWS CDK React App
+      </Typography>
+      <Typography
+        align="center"
+        variant="body1"
+        gutterBottom
+        style={{ fontWeight: 'bold', marginBottom: '2rem' }}
+      >
+        The following is a is a high-level overview of the architecture of the
+        react app. The basis of the app is that it is a serverless application
+        that uses AWS Lambda to handle the backend logic and DynamoDB to store
+        and manage the inventory data.
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            fullWidth
+            label="Name"
+            value={formData.name}
+            onChange={(event) => handleInputChange(event, 'name')}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            fullWidth
+            label="Email"
+            value={formData.email}
+            onChange={(event) => handleInputChange(event, 'email')}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            fullWidth
+            label="Phone"
+            value={formData.phone}
+            onChange={(event) => handleInputChange(event, 'phone')}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            fullWidth
+            label="Location"
+            name="location"
+            value={formData.location}
+            onChange={(event) => handleInputChange(event, 'location')}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            fullWidth
+            label="Discount Code"
+            value={formData.discount}
+            onChange={(event) => handleInputChange(event, 'discount')}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <Select
+              label="status"
+              name="status"
+              value={formData.status}
+              onChange={(event: any) => handleInputChange(event, 'status')}
+            >
+              <MenuItem value={Customer.RETAIL}>Retail</MenuItem>
+              <MenuItem value={Customer.WHOLESALE}>Wholesale</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <Select
+              label="memberType"
+              name="memberType"
+              value={formData.memberType}
+              onChange={(event: any) => handleInputChange(event, 'memberType')}
+            >
+              <MenuItem value={MemberType.MEMBER}>Member</MenuItem>
+              <MenuItem value={MemberType.NON_MEMBER}>Non-Member</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              width: '96.25%',
+              height: '70%',
+              borderRadius: 1,
+              border: '1px solid rgba(0, 0, 0, 0.23)',
+              padding: '8px 16px',
+              backgroundColor: 'white',
+              '&:hover': {
+                backgroundColor: 'rgba(0, 0, 0, 0.04)',
+              },
+              '&:focus-within': {
+                backgroundColor: 'rgba(0, 0, 0, 0.08)',
+                borderColor: '#80bdff',
+                boxShadow: '0 0 0 0.2rem rgba(0, 123, 255, 0.5)',
+              },
+            }}
+          >
+            <InputLabel id="memberType" sx={{ width: '80%' }}>
+              Customer Review
+            </InputLabel>
+
+            <Rating
+              name="review"
+              value={formData.review}
+              onChange={(event: any) => handleInputChange(event, 'review')}
+            />
+          </Box>
+        </Grid>
+        <Button
+          variant="contained"
+          fullWidth
+          onClick={handleAddProduct}
+          sx={{ marginTop: '1rem', marginBottom: '1rem' }}
+        >
+          Add Product
+        </Button>
+      </Grid>
       {formData.products.map((product, index) => (
         <div key={index}>
-          <TextField
-            label="Product ID"
-            value={product.id}
-            onChange={(event) => handleProductChange(event, 'id', index)}
-          />
-          <TextField
-            label="Product Name"
-            value={product.name}
-            onChange={(event) => handleProductChange(event, 'name', index)}
-          />
-          <TextField
-            label="Product Price"
-            type="number"
-            value={product.price}
-            onChange={(event) => handleProductChange(event, 'price', index)}
-          />
-          <TextField
-            label="Product Type"
-            value={product.productType}
-            onChange={(event) =>
-              handleProductChange(event, 'productType', index)
-            }
-          />
-          <Button
-            variant="contained"
-            onClick={() => handleRemoveProduct(index)}
-          >
-            Remove
-          </Button>
+          <Grid container spacing={2} columns={1}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                fullWidth
+                label="Product Name"
+                value={product.name}
+                onChange={(event) => handleProductChange(event, 'name', index)}
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                fullWidth
+                label="Product Type"
+                value={product.productType}
+                onChange={(event) =>
+                  handleProductChange(event, 'productType', index)
+                }
+              />
+            </Grid>
+            <Button
+              variant="contained"
+              fullWidth
+              onClick={() => handleRemoveProduct(index)}
+              sx={{ marginTop: '1rem', marginBottom: '1rem' }}
+            >
+              Remove
+            </Button>
+          </Grid>
         </div>
       ))}
-      <Button variant="contained" onClick={handleAddProduct}>
-        Add Product
-      </Button>
-      <br />
-      <TextField
-        label="Discount Code"
-        value={formData.discount}
-        onChange={(event) => handleInputChange(event, 'discount')}
-      />
-      <br />
-      <Button variant="contained" type="submit">
+
+      <Button
+        variant="contained"
+        type="submit"
+        fullWidth
+        color="success"
+        sx={{ marginTop: '1rem' }}
+      >
         Submit
       </Button>
+      <Info />
     </form>
   );
 };
